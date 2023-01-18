9f9a3f0ebb81a250e4a9e2c31408fe746daf570f
chandrey34 <vip034@ya.ru>
2023-01-18 23:52:24 +0300

    commit

--- project/src/main/java/com/sinx/project/ProjectFragment.kt
+++ project/src/main/java/com/sinx/project/ProjectFragment.kt
@@ -1,6 +1,38 @@
 package com.sinx.project
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sinx.project.adapter.ProjectListAdapter
+import com.sinx.project.data.ProjectListModel
 
 class ProjectFragment : Fragment(R.layout.project_layout) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.project_layout, container, false)
+
+        view.findViewById<TextView>(R.id.titleProject)
+
+        val recyclerView = view.findViewById(R.id.rvProjectList) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = ProjectListAdapter(dataListProject())
+
+        return view
+    }
+
+    private fun dataListProject(): ArrayList<ProjectListModel> {
+        val projectListModels = ArrayList<ProjectListModel>()
+        for (i in 0..100 )
+            projectListModels.add(ProjectListModel("Project Main", "01.02.2023"))
+        return projectListModels
+    }
 }
