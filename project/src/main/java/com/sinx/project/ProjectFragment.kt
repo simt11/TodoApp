ca5c62b7bac4611e947a2ed9ded4079e683d1888
chandrey34 <vip034@ya.ru>
2023-01-22 23:32:33 +0300

    * Поправил верстку (tools:text, layout_marginHorizontal ) * Добавил binding в ProjectFragment * Добавил binding в конструктор ProjectListViewHolder * Вынес colors.xml в core * Удалил themes.xml * В project_layout.xml заменил Relative на ConstraintLayout

--- project/src/main/java/com/sinx/project/ProjectFragment.kt
+++ project/src/main/java/com/sinx/project/ProjectFragment.kt
@@ -4,29 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.sinx.project.adapter.ProjectListAdapter
 import com.sinx.project.data.ProjectListModel
+import com.sinx.project.databinding.ProjectLayoutBinding
 
 class ProjectFragment : Fragment(R.layout.project_layout) {
+    lateinit var binding: ProjectLayoutBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.project_layout, container, false)
+    ): View {
+        binding = ProjectLayoutBinding.inflate(layoutInflater)
 
-        view.findViewById<TextView>(R.id.titleProject)
+        binding.rvProjectList.layoutManager = LinearLayoutManager(context)
+        binding.rvProjectList.adapter = ProjectListAdapter(dataListProject())
 
-        val recyclerView = view.findViewById(R.id.rvProjectList) as RecyclerView
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = ProjectListAdapter(dataListProject())
-
-        return view
+        return binding.root
     }
 
     private fun dataListProject(): ArrayList<ProjectListModel> {
--- project/src/main/java/com/sinx/project/adapter/ProjectListAdapter.kt
+++ project/src/main/java/com/sinx/project/adapter/ProjectListAdapter.kt
@@ -1,33 +1,30 @@
 package com.sinx.project.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.sinx.project.R
 import com.sinx.project.data.ProjectListModel
 import com.sinx.project.databinding.ListProjectBinding
+import com.sinx.core.R as core_R
 
-class ProjectListAdapter(private val projectListModels: ArrayList<ProjectListModel>) : RecyclerView.Adapter<ProjectListAdapter.ProjectListViewHolder>() {
 
-    class ProjectListViewHolder(item: View) : RecyclerView.ViewHolder(item) {
-        private val binding = ListProjectBinding.bind(item)
+internal class ProjectListAdapter(private val projectListModels: ArrayList<ProjectListModel>) : RecyclerView.Adapter<ProjectListAdapter.ProjectListViewHolder>() {
+
+    class ProjectListViewHolder(private val binding: ListProjectBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(projectListModel: ProjectListModel) {
             binding.nameProject.text = projectListModel.nameProject
             binding.dataProject.text = projectListModel.dataProject
-            binding.movingProject.setImageResource(com.sinx.core.R.drawable.moving)
-            binding.dividerProject.setImageResource(com.sinx.core.R.drawable.divider)
+            binding.movingProject.setImageResource(core_R.drawable.moving)
+            binding.dividerProject.setImageResource(core_R.drawable.divider)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectListViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_project, parent, false)
-        return ProjectListViewHolder(view)
+        val binding = ListProjectBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ProjectListViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return projectListModels.size
-    }
+    override fun getItemCount() = projectListModels.size
 
     override fun onBindViewHolder(holder: ProjectListViewHolder, position: Int) {
         holder.bind(projectListModels[position])
