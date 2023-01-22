f5d3c89641283452331a6bbf822250b3d16da609
aagvozdev <aagvozdev@avito.ru>
2023-01-22 17:11:12 +0300

    TASK-32: fix namespace

--- project/src/main/java/com/sinx/project/ProjectFragment.kt
+++ project/src/main/java/com/sinx/project/ProjectFragment.kt
@@ -1,7 +1,6 @@
 package com.sinx.project
 
 import androidx.fragment.app.Fragment
-//import com.sinx.todo.R
 
 class ProjectFragment : Fragment(R.layout.project_layout) {
 }
--- task/src/main/java/com/sinx/task/TaskListFragment.kt
+++ task/src/main/java/com/sinx/task/TaskListFragment.kt
@@ -1,7 +1,6 @@
 package com.sinx.task
 
 import androidx.fragment.app.Fragment
-//import com.sinx.todo.R
 
 class TaskListFragment : Fragment(R.layout.task_list_layout) {
 }
