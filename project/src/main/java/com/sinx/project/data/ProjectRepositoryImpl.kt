e8c6451bf6942b1ad62e174f081b85018a8d7666
SFF <Fail.Salikhov@rosbank.ru>
2023-05-12 00:07:46 +0500

    task- 33, правки

--- project/src/main/java/com/sinx/project/data/ProjectRepositoryImpl.kt
+++ project/src/main/java/com/sinx/project/data/ProjectRepositoryImpl.kt
@@ -11,26 +11,11 @@ internal class ProjectRepositoryImpl(
 
     private val mapper = Mapper()
 
-//    private var projectList = mutableListOf<ProjectListModel>()
-//    private val projectFlow = MutableSharedFlow<List<ProjectListModel>>(replay = 1)
-//    private val scope = CoroutineScope(Dispatchers.IO)
-
     override suspend fun addNewProject(newProject: ProjectListModel) {
         projectDao.addTask(mapper.mapProjectItemToProjectDb(newProject))
     }
 
     override suspend fun listTasksFlow(): Flow<List<ProjectListModel>> {
-//        if (projectList.isEmpty()) {
-//            for (i in 0..2) {
-//                val newProject = ProjectListModel(
-//                    nameProject = "Project Main ${i + 1}",
-//                    dataProject = "07 Jan 23"
-//                )
-//                projectList.add(newProject)
-//            }
-//        }
-//        projectFlow.emit(projectList)
-//        return projectFlow
         return projectDao.getTaskList().map {
             it.map {
                 mapper.mapProjectDbToProjectItem(it)
